4ed3e73be92cd661a24d29bc11feb6f9aa69f19c
Jesse Brent Coars <jbcoats@users.noreply.github.com>
2017-04-12 06:41:52 -0700

    Completed assignment.

---
+++ Assignment2/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="main.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>Calendar of Events</h1>
+    <form id="form1" runat="server">
+    <div">
+        <asp:Calendar ID="CalendarOfEvents" runat="server" OnSelectionChanged="CalendarOfEvents_SelectionChanged"></asp:Calendar>
+        <asp:Label ID="EventLabel" runat="server" Text="" CssClass="results"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Assignment2/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    string[,] CalendarEvents = new string[6, 2];
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        CalendarEvents[0, 0] = "4/30/2017";
+        CalendarEvents[0, 1] = "Jessie's Birthday";
+        CalendarEvents[1, 0] = "5/5/2017";
+        CalendarEvents[1, 1] = "Joshua's Birthday";
+        CalendarEvents[2, 0] = "5/5/2017";
+        CalendarEvents[2, 1] = "Cinco de Mayo";
+        CalendarEvents[3, 0] = "7/4/2017";
+        CalendarEvents[3, 1] = "Independance Day";
+        CalendarEvents[4, 0] = "12/25/2017";
+        CalendarEvents[4, 1] = "Christmas";
+        CalendarEvents[5, 0] = "1/1/2017";
+        CalendarEvents[5, 1] = "New Year's Day";
+    }
+
+    protected void CalendarOfEvents_SelectionChanged(object sender, EventArgs e)
+    {
+        string eventDate = CalendarOfEvents.SelectedDate.ToShortDateString();
+
+        EventLabel.Text = "";
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (eventDate.Equals(CalendarEvents[i, 0]))
+            {
+                EventLabel.Text += "<br />" + CalendarEvents[i, 1];
+            }
+        }
+
+    }
+}
